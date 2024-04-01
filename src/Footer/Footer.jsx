82a640516199e8f7499eda99fd9fe9c58739673a
KemalGokten <ukemalgokten@gmail.com>
2024-04-01 20:28:24 +0200

    Add Sidebar and Footer Component

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <img src="" alt="github logo" />
+            <a href="https://github.com/KemalGokten/to-do-app/tree/main">Github repository</a>
+        </footer>
+     );
+}
+ 
+export default Footer;

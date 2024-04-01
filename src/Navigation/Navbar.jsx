832431cd1d8ad70e991030632a7349d5c5343882
KemalGokten <ukemalgokten@gmail.com>
2024-04-01 20:27:46 +0200

    Add Navbar Component in Navbar.jsx

---
+++ src/Navigation/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <p className="app-name">To Do App</p>
+      <a className="app-logo" href="#">
+        <img src="" alt="App Logo" />
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;

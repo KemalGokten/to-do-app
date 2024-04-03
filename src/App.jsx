cae2eb371cfb526727686bcc5a032b0480293229
KemalGokten <ukemalgokten@gmail.com>
2024-04-03 18:46:31 +0200

    Change shown message on unknown page path

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Route path="/tasks" element={<MainContent />}></Route>
         <Route path="/tasks/:taskId" element={<DetailedTask/>}></Route>
         <Route path="/about" element={<h1>About</h1>}></Route>
-        <Route path="*" element={<h1>404</h1>}></Route>
+        <Route path="*" element={<h1>404 Page not found</h1>}></Route>
       </Routes>
     </div>
     <Footer />

32574b12742ca11068cbc92268be88e7a0f6f43e
KemalGokten <ukemalgokten@gmail.com>
2024-04-04 23:42:06 +0200

    Add Link to navigation to edit for selected task

--- src/components/DetailedTask.jsx
+++ src/components/DetailedTask.jsx
@@ -1,17 +1,18 @@
-import { useLocation } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom"; // Import Link from react-router-dom
 
 const DetailedTask = () => {
-    const location = useLocation();
-    const task = location.state.task;
-    console.log(task);
-    return ( 
-        <div>
-            <p>Task id: {task.id}</p>
-            <p>Task: {task.task}</p>
-            <p>Task status: {task.completed.toString()}</p>
-        </div>
-     );
-}
- 
+  const location = useLocation();
+  const task = location.state.task;
+  return (
+    <div>
+      <p>Task id: {task.id}</p>
+      <p>Task: {task.task}</p>
+      <p>Task status: {task.completed.toString()}</p>
+      <Link to={`/tasks/${task.id}/edit`} state={{ task }}>
+        <button>Edit</button>
+      </Link>
+    </div>
+  );
+};
+
 export default DetailedTask;

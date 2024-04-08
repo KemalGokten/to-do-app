e1ffabf93e3107e32623680942baa4ac03ca558f
KemalGokten <ukemalgokten@gmail.com>
2024-04-08 15:55:58 +0200

    Add logo to footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+import githubLogo from "../assets/images/github-logo.png"
+
 const Footer = () => {
     return ( 
         <footer className="footer">
-            <img src="" alt="github logo" />
+            <img src={githubLogo} alt="github logo" />
             <a href="https://github.com/KemalGokten/to-do-app/tree/main">Github repository</a>
         </footer>
      );

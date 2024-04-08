7d9cd1681fb3de8f87a24aefb647378ae6121dd3
KemalGokten <ukemalgokten@gmail.com>
2024-04-08 15:56:10 +0200

    Add app logo to navbar

--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
+import appLogo from "../assets/images/app-logo.png"
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <p className="app-name">To Do App</p>
       <Link to={`/tasks`}>
-        <img src="" alt="App Logo" />
+        <img className="app-logo" src={appLogo} alt="App Logo" />
       </Link>
     </nav>
   );
